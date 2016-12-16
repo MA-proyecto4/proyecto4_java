99bbfe5f4d745f180dc889f34be0967f76885654
Aaron4786 <4786.joan23@fje.edu>
2016-12-16 18:28:18 +0100

    Solución conflicto con la BD

--- bd_estoc.sql
+++ bd_estoc.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-12-2016 a las 17:21:30
--- Versión del servidor: 10.1.9-MariaDB
--- Versión de PHP: 5.6.15
+-- Tiempo de generación: 16-12-2016 a las 18:27:48
+-- Versión del servidor: 10.1.13-MariaDB
+-- Versión de PHP: 7.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `bd_estoc`
 --
-CREATE DATABASE IF NOT EXISTS `bd_estoc` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-USE `bd_estoc`;
 
 -- --------------------------------------------------------
 
@@ -28,21 +26,21 @@ USE `bd_estoc`;
 -- Estructura de tabla para la tabla `tbl_categoria`
 --
 
-DROP TABLE IF EXISTS `tbl_categoria`;
-CREATE TABLE IF NOT EXISTS `tbl_categoria` (
-  `categoria_id` int(11) NOT NULL AUTO_INCREMENT,
-  `categoria_nom` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`categoria_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE `tbl_categoria` (
+  `categoria_id` int(11) NOT NULL,
+  `categoria_nom` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `tbl_categoria`
 --
 
 INSERT INTO `tbl_categoria` (`categoria_id`, `categoria_nom`) VALUES
-(1, 'Mueble'),
-(2, 'Fruta'),
-(3, 'Informatico');
+(4, 'Mesa'),
+(5, 'Silla'),
+(6, 'Lámpara'),
+(8, 'Sofá'),
+(9, 'Escritorio');
 
 -- --------------------------------------------------------
 
@@ -50,14 +48,11 @@ INSERT INTO `tbl_categoria` (`categoria_id`, `categoria_nom`) VALUES
 -- Estructura de tabla para la tabla `tbl_comanda`
 --
 
-DROP TABLE IF EXISTS `tbl_comanda`;
-CREATE TABLE IF NOT EXISTS `tbl_comanda` (
-  `comanda_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_comanda` (
+  `comanda_id` int(11) NOT NULL,
   `comanda_quantitat` int(5) NOT NULL,
   `comanda_data` date NOT NULL,
-  `prov_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`comanda_id`),
-  KEY `FK_comanda_prov` (`prov_id`)
+  `prov_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -66,14 +61,10 @@ CREATE TABLE IF NOT EXISTS `tbl_comanda` (
 -- Estructura de tabla para la tabla `tbl_detall_com`
 --
 
-DROP TABLE IF EXISTS `tbl_detall_com`;
-CREATE TABLE IF NOT EXISTS `tbl_detall_com` (
-  `detall_com_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_detall_com` (
+  `detall_com_id` int(11) NOT NULL,
   `comanda_id` int(11) NOT NULL,
-  `prod_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`detall_com_id`),
-  KEY `FK_detall_com_prod` (`prod_id`),
-  KEY `FK_detall_com_comanda` (`comanda_id`)
+  `prod_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -82,26 +73,30 @@ CREATE TABLE IF NOT EXISTS `tbl_detall_com` (
 -- Estructura de tabla para la tabla `tbl_estoc`
 --
 
-DROP TABLE IF EXISTS `tbl_estoc`;
-CREATE TABLE IF NOT EXISTS `tbl_estoc` (
-  `estoc_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_estoc` (
+  `estoc_id` int(11) NOT NULL,
   `estoc_q_max` int(5) NOT NULL,
   `estoc_q_min` int(5) NOT NULL,
   `prod_id` int(11) DEFAULT NULL,
   `lloc_id` int(11) DEFAULT NULL,
-  `estoc_actual` int(4) NOT NULL,
-  PRIMARY KEY (`estoc_id`),
-  KEY `FK_estoc_prod` (`prod_id`),
-  KEY `FK_estoc_lloc` (`lloc_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `estoc_actual` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `tbl_estoc`
 --
 
 INSERT INTO `tbl_estoc` (`estoc_id`, `estoc_q_max`, `estoc_q_min`, `prod_id`, `lloc_id`, `estoc_actual`) VALUES
-(1, 99, 10, 2, 1, 14),
-(2, 79, 5, 3, 1, 4);
+(3, 80, 20, 4, 1, 55),
+(4, 50, 10, 5, 1, 22),
+(5, 120, 30, 6, 1, 48),
+(6, 150, 35, 7, 1, 143),
+(7, 60, 15, 8, 1, 56),
+(8, 90, 20, 9, 1, 65),
+(9, 300, 50, 10, 1, 289),
+(10, 45, 10, 11, 1, 13),
+(11, 150, 25, 12, 1, 83),
+(12, 250, 35, 13, 1, 89);
 
 -- --------------------------------------------------------
 
@@ -109,14 +104,12 @@ INSERT INTO `tbl_estoc` (`estoc_id`, `estoc_q_max`, `estoc_q_min`, `prod_id`, `l
 -- Estructura de tabla para la tabla `tbl_lloc`
 --
 
-DROP TABLE IF EXISTS `tbl_lloc`;
-CREATE TABLE IF NOT EXISTS `tbl_lloc` (
-  `lloc_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_lloc` (
+  `lloc_id` int(11) NOT NULL,
   `num_bloc` enum('Bloc 1','Bloc 2','Bloc3','Bloc 4','Bloc 5','Bloc 6','Bloc 7','Bloc 8') COLLATE utf8_unicode_ci NOT NULL,
   `num_passadis` enum('passadis 1','passadis 2','passadis 3','passadis 4','passadis 5','passadis 6','passadis 7','passadis 8','passadis 9','passadis 10','passadis 11','passadis 12') COLLATE utf8_unicode_ci NOT NULL,
-  `num_lleixa` enum('lleixa 1','lleixa 2','lleixa 3','lleixa 4','lleixa 5','lleixa 6','lleixa 7','lleixa 8') COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`lloc_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `num_lleixa` enum('lleixa 1','lleixa 2','lleixa 3','lleixa 4','lleixa 5','lleixa 6','lleixa 7','lleixa 8') COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `tbl_lloc`
@@ -131,23 +124,29 @@ INSERT INTO `tbl_lloc` (`lloc_id`, `num_bloc`, `num_passadis`, `num_lleixa`) VAL
 -- Estructura de tabla para la tabla `tbl_producte`
 --
 
-DROP TABLE IF EXISTS `tbl_producte`;
-CREATE TABLE IF NOT EXISTS `tbl_producte` (
-  `prod_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_producte` (
+  `prod_id` int(11) NOT NULL,
   `prod_nom` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `prod_foto` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `prod_precio` double(5,2) NOT NULL,
-  `categoria_id` int(11) NOT NULL,
-  PRIMARY KEY (`prod_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `categoria_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `tbl_producte`
 --
 
 INSERT INTO `tbl_producte` (`prod_id`, `prod_nom`, `prod_foto`, `prod_precio`, `categoria_id`) VALUES
-(2, 'prueba', 'NA', 4.00, 1),
-(3, 'prueba2', 'NA', 5.00, 2);
+(4, 'Silla minimalista de polipiel ', '', 35.00, 5),
+(5, 'Silla salón tapizada', '', 49.00, 5),
+(6, 'Mesa comedor tapa cristal', '', 86.00, 4),
+(7, 'Mesa cristal fija', '', 132.00, 4),
+(8, 'Sofá cama libro', '', 166.00, 8),
+(9, 'Sofá desenfundable 3 plazas', '', 226.00, 8),
+(10, 'Mesa de oficina New Pano', '', 108.00, 9),
+(11, 'LEA Escritorio de madera', '', 249.90, 9),
+(12, 'Lámpara de pie DIVA LED 2', '', 95.00, 6),
+(13, 'Lámpara colgante CARCASS ', '', 60.00, 6);
 
 -- --------------------------------------------------------
 
@@ -155,13 +154,11 @@ INSERT INTO `tbl_producte` (`prod_id`, `prod_nom`, `prod_foto`, `prod_precio`, `
 -- Estructura de tabla para la tabla `tbl_proveidor`
 --
 
-DROP TABLE IF EXISTS `tbl_proveidor`;
-CREATE TABLE IF NOT EXISTS `tbl_proveidor` (
-  `prov_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_proveidor` (
+  `prov_id` int(11) NOT NULL,
   `prov_nom` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
   `prov_adre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `prov_email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`prov_id`)
+  `prov_email` varchar(100) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -170,21 +167,122 @@ CREATE TABLE IF NOT EXISTS `tbl_proveidor` (
 -- Estructura de tabla para la tabla `tbl_usuario`
 --
 
-DROP TABLE IF EXISTS `tbl_usuario`;
-CREATE TABLE IF NOT EXISTS `tbl_usuario` (
-  `usu_id` int(3) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `tbl_usuario` (
+  `usu_id` int(3) NOT NULL,
   `usu_nick` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
-  `usu_pass` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`usu_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `usu_pass` varchar(25) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `tbl_usuario`
 --
 
 INSERT INTO `tbl_usuario` (`usu_id`, `usu_nick`, `usu_pass`) VALUES
-(1, 'mlopez', 'asdf');
+(1, 'mlopez', 'asdf'),
+(2, 'amunoz', '1234'),
+(3, 'sjimenez', 'qwe123');
 
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tbl_categoria`
+--
+ALTER TABLE `tbl_categoria`
+  ADD PRIMARY KEY (`categoria_id`);
+
+--
+-- Indices de la tabla `tbl_comanda`
+--
+ALTER TABLE `tbl_comanda`
+  ADD PRIMARY KEY (`comanda_id`),
+  ADD KEY `FK_comanda_prov` (`prov_id`);
+
+--
+-- Indices de la tabla `tbl_detall_com`
+--
+ALTER TABLE `tbl_detall_com`
+  ADD PRIMARY KEY (`detall_com_id`),
+  ADD KEY `FK_detall_com_prod` (`prod_id`),
+  ADD KEY `FK_detall_com_comanda` (`comanda_id`);
+
+--
+-- Indices de la tabla `tbl_estoc`
+--
+ALTER TABLE `tbl_estoc`
+  ADD PRIMARY KEY (`estoc_id`),
+  ADD KEY `FK_estoc_prod` (`prod_id`),
+  ADD KEY `FK_estoc_lloc` (`lloc_id`);
+
+--
+-- Indices de la tabla `tbl_lloc`
+--
+ALTER TABLE `tbl_lloc`
+  ADD PRIMARY KEY (`lloc_id`);
+
+--
+-- Indices de la tabla `tbl_producte`
+--
+ALTER TABLE `tbl_producte`
+  ADD PRIMARY KEY (`prod_id`);
+
+--
+-- Indices de la tabla `tbl_proveidor`
+--
+ALTER TABLE `tbl_proveidor`
+  ADD PRIMARY KEY (`prov_id`);
+
+--
+-- Indices de la tabla `tbl_usuario`
+--
+ALTER TABLE `tbl_usuario`
+  ADD PRIMARY KEY (`usu_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_categoria`
+--
+ALTER TABLE `tbl_categoria`
+  MODIFY `categoria_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT de la tabla `tbl_comanda`
+--
+ALTER TABLE `tbl_comanda`
+  MODIFY `comanda_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tbl_detall_com`
+--
+ALTER TABLE `tbl_detall_com`
+  MODIFY `detall_com_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tbl_estoc`
+--
+ALTER TABLE `tbl_estoc`
+  MODIFY `estoc_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT de la tabla `tbl_lloc`
+--
+ALTER TABLE `tbl_lloc`
+  MODIFY `lloc_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `tbl_producte`
+--
+ALTER TABLE `tbl_producte`
+  MODIFY `prod_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
+-- AUTO_INCREMENT de la tabla `tbl_proveidor`
+--
+ALTER TABLE `tbl_proveidor`
+  MODIFY `prov_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tbl_usuario`
+--
+ALTER TABLE `tbl_usuario`
+  MODIFY `usu_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- Restricciones para tablas volcadas
 --
